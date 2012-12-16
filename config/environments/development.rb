4d1b79940bb15edb56ea0114e018620af1899d0a
Cameron J <cdjuarez@ucsd.edu>
2012-12-16 12:46:51 -0800

    Devise.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,11 @@ CamjuarezCom::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Devise config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+
+  # For Heroku
+  config.assets.initialize_on_precompile = false
+
 end
