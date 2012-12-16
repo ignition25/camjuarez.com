b8d14dc41aaeaabb5c17e3e0e55c3d900b188f66
Cameron J <cdjuarez@ucsd.edu>
2012-12-16 12:39:22 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CamjuarezCom::Application.config.secret_token = 'bed1d3ee5dc5b386303b11daf025d301889f25d74620c1835c0d3cdacfcdce2c17f97baf24f2b40905fd9352e568c95bdfe20dcb4cec0ca02028b4288d352d7d'
