51ea11adeeca27c05bba5e8be0ae76b3d6dbed51
Cameron J <cdjuarez@ucsd.edu>
2012-12-17 22:29:35 -0800

    Latest updates.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, :only => [:index, :new, :edit, :create, :update, :destroy]
   # GET /posts
   # GET /posts.json
   def index
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
-  attr_accessible :entry, :title
+	include ActiveRecord::Timestamp
+  	attr_accessible :entry, :title
 end
