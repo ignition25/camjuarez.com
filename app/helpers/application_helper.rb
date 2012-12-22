826b708c956d268566fc7095a4e3c805c16d5756
Cameron J <cdjuarez@ucsd.edu>
2012-12-22 15:09:46 -0800

    Minor tweaks.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def coderay(text)
-      text.gsub(/\<pre( lang="(.+?)")?\><code( lang="(.+?)")?\>(.+?)\<\/code\><\/pre\>/m) do
+      text.gsub(/\<pre( lang="(.+?)")?\>\<code( lang="(.+?)")?\>(.+?)\<\/code\>\<\/pre\>/m) do
       	CodeRay.scan(CGI.unescapeHTML($5), $2).div(:css => :class)
       end
   end
