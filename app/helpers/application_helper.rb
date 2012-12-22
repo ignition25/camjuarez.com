383aa53176eea04ea159390cc559b8abf3a78f60
Cameron J <cdjuarez@ucsd.edu>
2012-12-22 14:35:39 -0800

    Quick fix to syntax highlighting.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def coderay(text)
       text.gsub(/\<pre( lang="(.+?)")?\><code( lang="(.+?)")?\>(.+?)\<\/code\><\/pre\>/m) do
-        CGI.unescapeHTML(CodeRay.scan($5, $2).div(:css => :class))
+      	CodeRay.scan(CGI.unescapeHTML($5), $2).div(:css => :class)
       end
   end
 end
