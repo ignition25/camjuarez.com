f22816cd01490e64df94eb6334a460023e8225c4
Cameron J <cdjuarez@ucsd.edu>
2013-01-01 13:16:25 -0800

    Added timeago.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,27 @@ module ApplicationHelper
   	"Cam Juarez is a computer science and math double major, software engineer, web dev, and music lover, 
   	who enjoys sharing interesting findings and elegant solutions."
   end
+
+  def timeago(time)
+    time_ago = ((Time.now - time) / 86400)
+    # Less than a day ago
+    if time_ago < 1 
+      time_ago *= 24
+    # Less than an hour ago
+      if time_ago < 1
+        time_ago *= 60
+    # Less than a minute ago
+        if time_ago < 1
+          time_ago *= 60
+          return "#{time_ago = time_ago.round}" + ((time_ago == 1) ? " second ago" :  " seconds ago")
+        end
+        return "#{time_ago = time_ago.round}" + ((time_ago == 1) ? " minute ago" :  " minutes ago")
+      end
+      return "#{time_ago = time_ago.round}" + ((time_ago == 1) ? " hour ago" :  " hours ago")
+    elsif time_ago == 1
+      return "#{time_ago = time_ago.round} day ago"
+    else
+      return "#{time_ago = time_ago.round} days ago"
+    end
+  end
 end
